2a98a923ead5a31abf06129b26c3a0e8e223a597
Ian Núñez <53503150+IanrolandoNuRi@users.noreply.github.com>
2024-03-15 22:37:38 -0500

    feature(add routes and clean files) (#2)

--- calc_backend/src/index.ts
+++ calc_backend/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express'
+
+import sumRouter from './routes/sum'
+
 const app = express()
 app.use(express.json())
 
-const PORT=3000
+const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('someone pinged here!!')
-    res.send('pong a')
+  console.log('someone pinged here!!')
+  res.send('pong')
 })
 
+app.use('/api/sum', sumRouter)
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
